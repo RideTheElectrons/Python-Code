dd385b96ce9cb8b5d2ece2290265d9bde8669286
George-Bogdan Nastase <45919198+RideTheElectrons@users.noreply.github.com>
2025-08-20 17:56:06 +0300

    Add files via upload

---
+++ AddingNoise.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+#gaussian_noise = np.random.normal(mean, std_deviation, shape)
+
+
+
+#Creating an independent variable
+
+x = np.arange(0,100,1)
+
+
+
+#Use a linear function to obtain the dependent variable
+
+y = 0.3*x + 0.6
+
+#Noise generation
+noise = np.random.normal(0,2,len(x))
+
+
+
+#Add the noise to the data
+
+y_noised = y + noise
+
+
+
+print(x)
+
+
+print(y)
+
+
+print(y_noised)
